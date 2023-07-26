5e112fa99be34c2f90ab0b3266ce7cd04727e708
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-07-26 10:25:23 -0700

    Initial commit

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ lib, rustPlatform, stdenv, darwin }:
+
+let cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+
+in rustPlatform.buildRustPackage rec {
+  pname = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
+  version =
+    (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+
+  src = ./.;
+
+  cargoHash = "sha256-VlEamDg5c1Hc03z5/7YWekY23ufkWCgyeOfAOtYjuFY=";
+
+  buildInputs = [ ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.IOKit
+    darwin.apple_sdk.frameworks.Security
+  ];
+
+  env = { VERGEN_IDEMPOTENT = true; };
+
+  meta = with lib; {
+    description = "The server-side software for Remote Text";
+    homepage = "https://github.com/Remote-Text/remote-text-server";
+    license = with licenses; [ ];
+    maintainers = with maintainers; [ ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "The server-side software for Remote Text";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = gen:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
+        (system: gen nixpkgs.legacyPackages.${system});
+    in {
+      packages = forAllSystems (pkgs: { default = pkgs.callPackage ./. { }; });
+
+      formatter = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
+        (system: nixpkgs.legacyPackages.${system}.nixfmt);
+    };
+}
