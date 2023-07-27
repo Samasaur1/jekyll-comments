92d65e66ecc9c342d256390c740322fb11a892c4
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-07-27 11:31:07 -0700

    Properly expose nixos module

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     in {
       packages = forAllSystems (pkgs: { default = pkgs.callPackage ./. { }; });
 
-      modules.default = import ./module.nix;
+      nixosModules.default = import ./module.nix;
 
       formatter = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
         (system: nixpkgs.legacyPackages.${system}.nixfmt);
